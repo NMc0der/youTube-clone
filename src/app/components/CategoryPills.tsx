835f210bb481d9cb98142a184214644dd852ba03
Noah <noahglennmaldonado@gmail.com>
2025-04-24 10:06:02 -0400

    finished with category pills section

---
+++ src/app/components/CategoryPills.tsx
@@ -0,0 +1,110 @@
+// "use client";
+
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import React, { useEffect } from "react";
+import { useState, useRef } from "react";
+
+type CategoryPillProps = {
+  categories: string[];
+  selectedCategory: string;
+  onSelect: (category: string) => void;
+};
+
+const TrasnlateAmount = 200;
+
+const CategoryPills = ({
+  categories,
+  selectedCategory,
+  onSelect,
+}: CategoryPillProps) => {
+  const [translate, setTranslate] = useState(1);
+  const [isLeftVisible, setIsLeftVisible] = useState(true);
+  const [isRightVisible, setIsRightVisible] = useState(true);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (containerRef.current == null) return;
+    const observer = new ResizeObserver((entries) => {
+      const container = entries[0]?.target;
+      if (container == null) return;
+      setIsLeftVisible(translate > 0);
+      setIsRightVisible(
+        translate + container.clientWidth < container.scrollWidth
+      );
+    });
+
+    observer.observe(containerRef.current);
+
+    return () => {
+      observer.disconnect;
+    };
+  }, [categories, translate]);
+
+  return (
+    <div ref={containerRef} className="overflow-x-hidden relative ">
+      <div className="flex whitespace-nowrap gap-3 transition-transform w-[max-content]">
+        {categories.map((category) => (
+          <button
+            key={category}
+            onClick={() => onSelect(category)}
+            className={`inline py-1 px-3 rounded-lg ${
+              selectedCategory === category
+                ? "bg-neutral-900 text-white"
+                : "bg-neutral-200 hover:bg-neutral-300"
+            } whitespace-nowrap bg-neutral-200  text-center cursor-pointer`}
+            style={{ transform: `translateX(-${translate}px)` }}
+          >
+            {category}
+          </button>
+        ))}
+
+        <button className="inline py-1 px-3 rounded-lg whitespace-nowrap bg-neutral-200 hover:bg-neutral-300 text-center cursor-pointer">
+          JavaScript
+        </button>
+      </div>
+      {isLeftVisible && (
+        <div className=" absolute left-0 top-1/2 -translate-y-51/100 bg-gradient-to-r from-white from-50% to-transparent w-24 h-full">
+          <button
+            onClick={() => {
+              setTranslate((translate) => {
+                const newTranslate = translate - TrasnlateAmount;
+                if (newTranslate <= 0) return 0;
+                return newTranslate;
+              });
+            }}
+            className="h-full aspect-square w-auto p-1.5 cursor-pointer "
+          >
+            <ChevronLeft />
+          </button>
+        </div>
+      )}
+      {isRightVisible && (
+        <div className="absolute right-0 top-1/2 -translate-y-51/100 bg-gradient-to-l from-white from-50% to-transparent w-24 h-full flex justify-end">
+          <button
+            onClick={() => {
+              setTranslate((translate) => {
+                if (containerRef.current == null) {
+                  console.log("hii");
+                  return translate;
+                }
+                console.log("yo");
+                const newTranslate = translate + TrasnlateAmount;
+                const edge = containerRef.current.scrollWidth;
+                const width = containerRef.current.clientWidth;
+                if (newTranslate + width >= edge) {
+                  return edge - width;
+                }
+                return newTranslate;
+              });
+            }}
+            className="h-full aspect-square w-auto p-1.5 "
+          >
+            <ChevronRight />
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CategoryPills;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,29 @@
+"use client";
+
 import Image from "next/image";
 import { PageHeader } from "./components/PageHeader";
+import CategoryPills from "./components/CategoryPills";
+import { categories } from "./data/home";
+import { useState } from "react";
 
 export default function Home() {
+  const [selectedCategory, setSelectedCategory] = useState(categories[0]);
+
   return (
     <div className="max-h-screen flex flex-col">
       <PageHeader />
-      <div className="gird grid-cols-[auto,1fr] flex-grow-1 overflow-auto"></div>
+      <div className="grid grid-cols-[auto_1fr] flex-grow-1 overflow-auto">
+        <div className="">Sidebar</div>
+        <div className="overflow-x-hidden px-8 pb-4">
+          <div className="sticky top-0 bg-white z-10 pb-4">
+            <CategoryPills
+              categories={categories}
+              selectedCategory={selectedCategory}
+              onSelect={setSelectedCategory}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
